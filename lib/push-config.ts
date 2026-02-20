3b676dfc291807859ad4422f0fb75714a8a445ad
marcin2121 <marcin2121@users.noreply.github.com>
2026-02-20 10:22:21 +0100

    Zmiana nazwy

--- lib/push-config.ts
+++ lib/push-config.ts
@@ -3,7 +3,7 @@
 export const PUSH_CATEGORIES = [
     { id: 'wszystkie', label: 'Wszystkie powiadomienia' }, // Zawsze zostawiamy
     { id: 'urwis', label: 'Sklep Urwis' },
-    { id: 'lecewkulki', label: 'Sala Zabaw' },
+    { id: 'lecewkulki', label: 'Sala Zabaw Lecę w Kulki' },
     // Możesz tu dodać cokolwiek w przyszłości i zmieni się wszędzie!
   ] as const;
   
